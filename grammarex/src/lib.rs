127c6754f5a527a993f6ca36c7fa8a27c0c0608e
Elchanan Haas <haas2376@gmail.com>
2024-05-01 11:56:51 -0700

    Start of parser

--- grammarex/src/lib.rs
+++ grammarex/src/lib.rs
@@ -1,8 +1,15 @@
-use std::ops::Range;
+use std::ops::{Range, RangeInclusive};
 
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum GrammarexParseError {
+    #[error("Missing closing backet")]
+    MissingClosingBracket,
+}
 enum CharClass {
     Char(char),
-    CharRange(Range<char>),
+    CharRange(RangeInclusive<char>),
     Union(Vec<CharClass>),
 }
 
@@ -13,6 +20,52 @@ impl CharClass {
             CharClass::CharRange(r) => r.contains(&c),
             CharClass::Union(classes) => classes.into_iter().any(|class| class.contains(c)),
         }
+    }
+}
+
+fn take_first(input: &mut &str) -> Option<char> {
+    let first: char = input.chars().next()?;
+    *input = &input[first.len_utf8()..];
+    return Some(first);
+}
+
+fn peek_k<const N: usize>(input: &&str) -> Option<[char; N]>{
+    let mut result: [char; N] = ['_'; N];
+    let mut chars = input.chars();
+    for i in 0..N {
+        let char = chars.next()?;
+        result[i] = char;
+    }
+    return Some(result);
+}
+fn peek(input: &&str, amount: usize) -> Option<char> {
+    let item = input.chars().skip(amount).next()?;
+    return Some(item);
+}
+fn skip(input: &mut &str, amount: usize) {
+    for _ in 0..amount {
+        let c = input.chars().next().expect("Uexpectedly ran out of input");
+        *input = &input[c.len_utf8()..];  
+    }
+}
+fn parse_backet_inner(input: &mut &str) -> Result<CharClass, GrammarexParseError> {
+    let mut classes = Vec::new();
+    let mut first = true;
+    loop {
+        let c = take_first(input).ok_or(GrammarexParseError::MissingClosingBracket)?;
+        //A regex backet can match the ']' char if it is the first char in the [] expression.
+        if c == ']' && !first {
+            return Ok(CharClass::Union(classes));
+        }
+        //[a-c] represents [abc] but if the dash is first or last, it is considered a literal like in [ax-]
+        if let Some(cs) = peek_k::<2>(input) {
+            if cs[0] == '-' && cs[1] != ']' {
+                classes.push(CharClass::CharRange(c..=cs[2]));
+                skip(input, 2);
+            }
+        }
+        classes.push(CharClass::Char(c));
+        first = false;
     }
 }
 pub fn add(left: usize, right: usize) -> usize {
